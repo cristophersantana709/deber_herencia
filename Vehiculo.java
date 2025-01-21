9669f347c0946b92b4abdd752a472ea590357213
cristophersantana709 <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2025-01-20 23:17:56 -0500

    Update Vehiculo.java

--- Vehiculo.java
+++ Vehiculo.java
@@ -2,4 +2,26 @@ package deber_herencia;
 
 public class Vehiculo {
 
+	
+	public String marca;
+	public String modelo;
+	public int anio;
+	
+	
+	public Vehiculo(String marca, String modelo, int anio) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.anio = anio;
+	}
+	
+	public void imprimir() {
+		
+		System.out.println("####### Vehiculo #######");
+		System.out.println("Marca:" + this.marca +" "+ "Modelo:" + this.modelo +" "+ "Año:" + this.anio);
+		System.out.println("El vehículo está encendido");
+		System.out.println("El vehículo está apagado");
+	}
+
+	
 }
