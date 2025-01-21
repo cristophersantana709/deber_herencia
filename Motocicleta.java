bf05df57edc14fba9ec70447313b8e7c6a9cd15e
cristophersantana709 <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2025-01-20 23:17:24 -0500

    Update Motocicleta.java

--- Motocicleta.java
+++ Motocicleta.java
@@ -1,5 +1,26 @@
 package deber_herencia;
 
-public class Motocicleta {
+public class Motocicleta extends Vehiculo{
 
+	String tipoCasco;
+
+	public Motocicleta(String marca, String modelo, int anio, String tipoCasco) {
+		super(marca, modelo, anio);
+		this.tipoCasco = tipoCasco;
+	}
+	
+	@Override
+	public void imprimir() {
+	
+	System.out.println("####### Motocicleta #######");
+	System.out.println("Marca:" + this.marca +" "+ "Modelo:" + this.modelo +" "+ "Año:" + this.anio +" "+"Tipo de Casco:" + this.tipoCasco);
+	System.out.println("El vehículo está encendido");
+	System.out.println("La motocicleta está haciendo un caballito");
+	System.out.println("El vehículo está apagado");
+	System.out.println("");
+	
+	}
+  
 }
+
+
