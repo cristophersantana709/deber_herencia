0f602de02e3ff1ac4e8b8f901beaf93c0f8b62fe
cristophersantana709 <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2025-01-20 23:16:44 -0500

    Update Camion.java

--- Camion.java
+++ Camion.java
@@ -1,5 +1,26 @@
 package deber_herencia;
 
-public class Camion {
+public class Camion extends Vehiculo{
+	
+	int capacidadCarga;
 
+	public Camion(String marca, String modelo, int anio, int capacidadCarga) {
+		super(marca, modelo, anio);
+		this.capacidadCarga = capacidadCarga;
+	}
+
+	@Override
+	public void imprimir() {
+	
+	System.out.println("####### Camion #######");
+	System.out.println("Marca:" + this.marca +" "+ "Modelo:" + this.modelo +" "+ "Año:" + this.anio +" "+"La capacidad de carga es:" + this.capacidadCarga);
+	System.out.println("El vehículo está encendido");
+	System.out.println("El camión está descargando");
+	System.out.println("El vehículo está apagado");
+	System.out.println("");
+	
+	}
+	
 }
+
+
