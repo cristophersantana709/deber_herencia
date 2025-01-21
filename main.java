1f31c75a97375614d59785127f044be8e5891c1c
cristophersantana709 <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2025-01-20 23:18:33 -0500

    Update main.java

--- main.java
+++ main.java
@@ -2,9 +2,17 @@ package deber_herencia;
 
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public static void main(String[] args) {
+		
+		Auto obj1 = new Auto("toyota", "Corolla", 2022, 4);
+		obj1.imprimir();
+		
+		Motocicleta obj2 = new Motocicleta("Kawasaki", "Ninja", 2020, "Mondular");
+		obj2.imprimir();
+		
+		Camion obj3 = new Camion("Volvo", "FH", 2018, 26);
+		obj3.imprimir();
+		
 	}
 
 }
